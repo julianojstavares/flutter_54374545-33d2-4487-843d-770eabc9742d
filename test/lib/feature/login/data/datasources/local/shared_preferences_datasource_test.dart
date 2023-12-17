a3908d603fd718233bb5607fb36e4cb6ac17dc7c
Juliano J. S. Tavares <oi@julianotavares.dev>
2023-12-17 18:34:01 -0300

    Adding test comments

--- test/lib/feature/login/data/datasources/local/shared_preferences_datasource_test.dart
+++ test/lib/feature/login/data/datasources/local/shared_preferences_datasource_test.dart
@@ -4,6 +4,21 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../../../../../shared/test_data.dart';
 
+/*
+Os testes unitários do Flutter são executados em um ambiente diferente
+do ambiente de execução do aplicativo, o que significa que eles não têm
+acesso às implementações nativas dos plugins. Portanto, o SharedPreferences
+não pode ser usado diretamente nos testes unitários.
+
+Uma solução para isso é usar 
+
+SharedPreferences.setMockInitialValues({})
+
+para simular o SharedPreferences. 
+
+Isso permite o código seja testado sem depender da execução do app.
+ */
+
 void main() {
   group('SharedPreferencesDatasource', () {
     late SharedPreferencesDatasource datasource;
